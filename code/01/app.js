601d73d020736d1f2720a283d8ac74cf271c1461
KerstiJnd <kerstijoarand@gmail.com>
2025-10-15 13:39:00 +0300

    JavaScript harjutused

---
+++ code/01/app.js
@@ -0,0 +1,34 @@
+//alert(`Tere tulemast!`);
+
+//confirm("Kas tahaksid lõunale minna?");
+
+//let firstName = "Kersti";
+
+//firstName = "Miki";
+
+//alert(firstName);
+
+//firstName = prompt("Palun sisesta eesnimi");
+
+//alert(firstName);
+
+//let choice;
+
+//let choice = confirm("Vali ei või jah");
+
+//alert(choice);
+
+
+//const number1 = prompt ("Sisesta esimene number");
+//const number2 = prompt ("Sisesta teine number");
+
+//alert(number1 - number2);
+
+//let age = 30;
+//alert(age);
+//age += 1;
+//age = age +1;
+//alert(age);
+
+const password = prompt("Sisesta parool");
+alert(password == 123);
