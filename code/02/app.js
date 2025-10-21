80a0dba4b888ff3ccec1e1fbab46fa995e370d32
KerstiJnd <kerstijoarand@gmail.com>
2025-10-21 15:56:44 +0300

    Tingimuslausete harjutused

--- code/02/app.js
+++ code/02/app.js
@@ -38,11 +38,101 @@ alert("Temperatuur Fahrenheiti skaalal:" + F);
 
 //Tingimuslaused
 
-const color = prompt("Mis värvi on foori tuli?");
+/*const color = prompt("Mis värvi on foori tuli?");
 if(color === "roheline"){
     alert("Võid üle tee minna");
 }
 
 else{
     alert("Pead ootama");
+}
+
+   if(color === "roheline"){
+    alert("Võid edasi sõita");
+   } else if(color==="kollane"){
+    alert("Pead veel tükk aega ootama");
+   }else if(color==="punane"){
+    alert("Oota veel pisut");
+   }else {
+    alert("Sellist värvi ei tohiks fooris olla");
+   }
+*/
+
+//Parooli kontrollimine
+
+/*const password = prompt("Palun sisesta kasutaja parool");
+
+const existingPassword = "admin123";
+if(password === "existingPassword"){
+    alert("Tere tulemast!");
+}
+else{
+    alert("Vale parool");
+}
+
+if(password !== "existingPassword"){
+    alert("Vale parool");
+}
+else{
+    alert("Tere tulemast!");
+}
+*/
+
+//Vanuse kontroll
+
+/*const age = prompt("Palun sisesta enda vanus");
+
+if(age ==="18"){
+    alert("Oled täisealine!");
+}
+
+else{
+    alert("Oled alaealine!");
+}
+
+if(age <18){
+    alert("Oled alaealine!");
+}
+
+else{
+    alert("Oled täisealine!");
+}
+
+
+if (age< ageLimit) {
+    alert("oled alaealine");
+} else{
+    alert("Oled täisealine");
+}
+    */
+
+/*const age = Number(prompt("Sisesta oma vanus"));
+
+alert(typeof age);
+*/
+
+//Lihtne kalkulaator
+
+const number1 = Number(prompt("Sisesta esimene arv"));
+const number2 = Number(prompt("Sisesta teine arv"));
+
+const operator = prompt("Millist tehet soovid teha? (+, -, *, /");
+if (operator=== "+"){
+    alert(number1 + number2);
+}
+
+else if(operator === "-" ){
+    alert(number1 - number2);
+}
+
+else if(operator === "*"){
+    alert(number1 * number2);
+}
+
+else if(operator ==="/"){
+    alert(number1 / number2);
+}
+
+else {
+    alert("Seda funktsiooni kalkulaator veel ei toeta");
 }
