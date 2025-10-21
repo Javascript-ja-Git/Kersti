1c06602d1ea0a8f6896703ef1eb2ef3405d3e0f2
KerstiJnd <kerstijoarand@gmail.com>
2025-10-21 13:04:14 +0300

    Harjutused muutujatega

--- code/02/app.js
+++ code/02/app.js
@@ -1,3 +1,37 @@
-const name = prompt("Mis su nimi on?");
-alert("Tere " + name);
+//Tervituse programm
 
+/*const name = prompt("Mis su nimi on?");
+alert("Tere " + name);
+*/
+
+//Vanuse arvutamine
+
+//alert("Sinu vanus on:" + (2025 -prompt("Sisesta oma vanus")))
+/*const year = prompt("Sisesta oma sünniaasta");
+const currentYear = 2025;
+const age = currentYear - year;
+
+alert("Sinu vanus on:" + age);
+*/
+
+//Ristküliku pindala arvutamine
+
+/*const heigth = prompt("Sisesta ristküliku kõrgus");
+const width = prompt("Sisesta ristküliku laius");
+const area = width * heigth;
+alert("Ristküliku pindala on " + area);
+*/
+
+//Temperatuuride teisendamine
+
+//Esimene versioon
+/*const temperatureCelsius = prompt("Sisesta temperatuur Celsiuse kraadides");
+const temperatureFahrenheit = temperatureCelsius *9 / 5 + 32;
+alert("Temperatuur Fahrenheiti skaalal:" + temperatureFahrenheit);
+*/
+
+//Teine versioon
+/*const C = prompt("Sisesta temperatuur Celsiuse kraadides");
+const F = C * 9 / 5 + 32;
+alert("Temperatuur Fahrenheiti skaalal:" + F);
+*/
