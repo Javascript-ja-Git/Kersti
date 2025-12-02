09326281b82cf1d46eadd7f80b26c8625b8fa9f7
KerstiJnd <kerstijoarand@gmail.com>
2025-12-02 16:58:03 +0200

    Seitsmenda loengu teemad

---
+++ code/07/data.js
@@ -0,0 +1,37 @@
+const titles = [
+    `Esimene pealkiri`,
+    `Teine pealkiri`,
+    `Kolmas pealkiri`,
+    `Neljas pealkiri`,
+    `Viies pealkiri`
+];
+
+const posts = [
+    {
+        id: 1,
+        title: `Esimene pealkiri`,
+        content: `Minu esimene postitus, oujeee!`,
+        author: `Mari Maasikas`,
+    },
+
+    {
+        id: 2,
+        title: `Teine pealkiri`,
+        content: `Minu teine postitus.`,
+        author: `Mari Maasikas`,
+    },
+
+    {
+        id: 3,
+        title: `Kolmas pealkiri`,
+        content: `Minu kolmas postitus!`,
+        author: `Mari Maasikas`,
+    },
+    
+    {
+        id: 4,
+        title: `Neljas pealkiri`,
+        content: `Minu neljas postitus!`,
+        author: `Mari Maasikas`,
+    }
+];
